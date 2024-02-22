3a75c9d47fa76fb03056d6b35e2120d5a6afa28b
Nihal Chehade <nihalche@icloud.com>
2024-02-22 04:02:30 -0800

    done functionality

---
+++ app.js
@@ -0,0 +1,31 @@
+console.log("Let's get this party started!");
+const input = document.querySelector("#txtInput");
+const search = document.querySelector("#search");
+const remove = document.querySelector("#remove");
+const div = document.querySelector("#gifs");
+
+search.addEventListener("click", (e) => {
+    e.preventDefault();
+    appendGif(input.value);
+});
+
+remove.addEventListener("click", (e) => {
+    e.preventDefault();
+    div.innerHTML = "";
+});
+
+
+async function getGifUrl(term) {
+
+    const randomIdx = Math.floor(Math.random() * 50);
+    const res = await axios.get('http://api.giphy.com/v1/gifs/search', { params: { api_key: "nn6fqIXvbPAScAClArHzmFGi1oDNCksl", q: term } });
+    console.log(res);
+    return res.data.data[randomIdx].images.original.url;
+}
+
+async function appendGif(term) {
+    input.value = "";
+    const img = document.createElement('img');
+    img.src = await getGifUrl(term);
+    div.append(img);
+}
